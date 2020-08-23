949f7d8c8e516425805f752e3d62676b2b0c719d
Manami.Iizuka <meganeika47@gmail.com>
2020-08-23 08:03:09 +0000

    0823金井くんUP

--- mimamorukun/status/urls.py
+++ mimamorukun/status/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'status' 
+
 urlpatterns = [
     path('', views.index, name='index'),
+    path('update/<int:pk>', views.upd, name='update'), 
 ]
