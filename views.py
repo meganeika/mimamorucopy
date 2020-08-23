e7d6da7ab26407be9d5c163c06a61986bfa5816a
金井孝樹 <kouki061089@gmail.com>
2020-08-23 01:56:23 +0000

    新規ファイルをアップロード

---
+++ views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Member
+
+# Create your views here.
+
+
+def index(request):
+    member = Member.objects.order_by('id')
+    return render(request, 'status/index.html', {'member': member})
+  
+#**********************やっぱいる。　引数にpkないとダメ
+def upd(request, pk):
+    #pk(プライマリーキープライマリーキー)をもとに更新対象のMemberを取得、tにわたす。
+    t = Member.objects.get(pk=pk)
+
+    # homeボタンがクリックされた場合の処理
+    if 'home' in request.POST:
+        if t.status_home:
+            t.status_home = 'False'
+        else:
+            t.status_home = 'True'
+            
+    # meetingボタンがクリックされた場合の処理
+    if 'meeting' in request.POST:
+        if t.status_meeting:
+            t.status_meeting = 'False'
+        else:
+            t.status_meeting = 'True'
+            
+    # lunchボタンがクリックされた場合の処理
+    if 'lunch' in request.POST:
+        if t.status_lunch:
+            t.status_lunch = 'False'
+        else:
+            t.status_lunch = 'True'
+    
+    #save()でDBに反映される。
+    t.save()
+            
+    return redirect('status:index')
+    
