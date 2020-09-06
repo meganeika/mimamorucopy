bc4d86c8456384e405de548a331f0c9b4e18475c
Koki.Kanai <kouki061089@gmail.com>
2020-09-06 14:00:27 +0900

    index.html urls.py views.py更新　login.html追加

--- mimamorukun/status/views.py
+++ mimamorukun/status/views.py
@@ -2,47 +2,82 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Member
 
-# Create your views here.
+#ログインユーザーのプライマリーキー 　グローバル変数として宣言するとどのメソッドでも共通して使える。
+global user_pk
 
 
+
+#ログイン画面
+def login(request):
+    global user_pk
+  
+    #POSTメソッド＝ログイン画面でloginボタンを押したとき
+    if request.method == "POST":
+        username = request.POST['username']
+
+        #ユーザーが存在する場合
+        try:
+            user = Member.objects.get(name=username)
+            user_pk = user.pk
+            return redirect('status:index')
+          
+        #ユーザーが存在しない場合
+        except:
+            return render(request, 'status/login.html', {'error':'ユーザーが見つかりません'})  
+        
+    #GETメソッド＝ログイン画面が呼び出されるとき
+    return render(request, 'status/login.html')
+
+
+#ホーム画面呼び出し
 def index(request):
     member = Member.objects.order_by('id')
     return render(request, 'status/index.html', {'member': member})
 
-#**********************やっぱいる。　引数にpkないとダメ
-def upd(request, pk):
-    #pk(プライマリーキープライマリーキー)をもとに更新対象のMemberを取得、tにわたす。
-    t = Member.objects.get(pk=pk)
 
-    # workボタンがクリックされた場合の処理
-    if 'work' in request.POST:
-        if t.status_work:
-            t.status_work = 'False'
-        else:
-            t.status_work = 'True'
-
-    # homeボタンがクリックされた場合の処理
-    if 'home' in request.POST:
-        if t.status_home:
-            t.status_home = 'False'
-        else:
-            t.status_home = 'True'
-
-    # meetingボタンがクリックされた場合の処理
-    if 'meeting' in request.POST:
-        if t.status_meeting:
-            t.status_meeting = 'False'
-        else:
-            t.status_meeting = 'True'
-
-    # lunchボタンがクリックされた場合の処理
-    if 'lunch' in request.POST:
-        if t.status_lunch:
-            t.status_lunch = 'False'
-        else:
-            t.status_lunch = 'True'
-
-    #save()でDBに反映される。
-    t.save()
-
-    return redirect('status:index')
+#ステータス更新    (member_pkにはindex.htmlのmember.pkが引き渡される)
+def upd(request, member_pk):
+    global user_pk
+  
+    #更新対象が自分以外だったら更新しない。
+    if member_pk != user_pk:
+        member = Member.objects.order_by('id')
+        return render(request, 'status/index.html', {'member': member, 'error':'!!自分以外のステータスは更新できません!!'})
+    
+    #更新対象が自分だったら更新。
+    else:
+        #pk(プライマリーキー)をもとにMemberテーブルから自分データを取得、tにわたす。
+        t = Member.objects.get(pk=user_pk)
+    
+        # workボタンがクリックされた場合の処理
+        if 'work' in request.POST:
+            if t.status_work:
+                t.status_work = 'False'
+            else:
+                t.status_work = 'True'
+    
+        # homeボタンがクリックされた場合の処理
+        if 'home' in request.POST:
+            if t.status_home:
+                t.status_home = 'False'
+            else:
+                t.status_home = 'True'
+    
+        # meetingボタンがクリックされた場合の処理
+        if 'meeting' in request.POST:
+            if t.status_meeting:
+                t.status_meeting = 'False'
+            else:
+                t.status_meeting = 'True'
+    
+        # lunchボタンがクリックされた場合の処理
+        if 'lunch' in request.POST:
+            if t.status_lunch:
+                t.status_lunch = 'False'
+            else:
+                t.status_lunch = 'True'
+    
+        #save()でDBに反映される。
+        t.save()
+    
+        return redirect('status:index')
